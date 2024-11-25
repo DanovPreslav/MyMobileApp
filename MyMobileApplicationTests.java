df1ae612d5bb3d5c7aff4eb4463ef8070fc3969e
DanovPreslav <92785048+DanovPreslav@users.noreply.github.com>
2024-11-25 21:42:40 +0200

    Add files via upload

---
+++ MyMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.mymobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
